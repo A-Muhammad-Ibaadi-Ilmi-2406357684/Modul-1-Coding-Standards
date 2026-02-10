843ab71cb9b37d8ab886a91b125d6388c98c1c73
Muhammad Ibaadi Ilmi <ibaadijaya2@gmail.com>
2026-02-10 07:35:20 +0700

    Randomly generate product ID

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -6,12 +6,18 @@ import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.UUID;
 
 @Repository
 public class ProductRepository {
     private List<Product> productData = new ArrayList<>();
 
     public Product create(Product product){
+        if (product.getProductId() == null) {
+            product.setProductId(UUID.randomUUID().toString());
+        } else {
+            throw new IllegalArgumentException("Product id must be null");
+        }
         productData.add(product);
         return product;
     }
