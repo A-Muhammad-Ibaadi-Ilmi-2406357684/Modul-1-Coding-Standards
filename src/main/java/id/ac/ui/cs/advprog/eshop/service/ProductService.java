7096abda104d8294b90dfec2eaf0e7611119e34c
Muhammad Ibaadi Ilmi <ibaadijaya2@gmail.com>
2026-02-24 20:48:29 +0700

    code quality: Remove redundant public modifiers in interfaces

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
@@ -4,8 +4,8 @@ import id.ac.ui.cs.advprog.eshop.model.Product;
 import java.util.List;
 
 public interface ProductService {
-    public Product save(Product product);
-    public List<Product> findAll();
-    public Product findById(String productId);
-    public boolean deleteById(String productId);
+    Product save(Product product);
+    List<Product> findAll();
+    Product findById(String productId);
+    boolean deleteById(String productId);
 }
